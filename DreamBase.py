e29dccad3b2ee6e0ec5a3efe2fffae7457849d27
daydreamingwill <williamlatham13@yahoo.co.uk>
2016-12-16 19:44:50 +0000

    Code refactored and files organisation improved
    Removed the "People_classes" package and the "Main" module. This was to
    reduce clutter and unnescessary complexity.
    
    All existing code has been refactored, in order to ensure that it
    complies with PEP8. Code from "Main" module has been moved into "People"
    module, which now exists in the "DreamBase" directory.

--- DreamBase.py
+++ DreamBase.py
@@ -1,9 +1,4 @@
-import sys
-sys.path.insert(0, 'C:\Users\willi_000\Desktop\Project DreamBase\People')
-from People_classes import *
-
 with open('Saved_objects.txt', 'r') as Saved_objects:
-
-    object_dictionary =  eval('Saved_objects').read()
+    object_dictionary = eval('Saved_objects').read()
 
 
